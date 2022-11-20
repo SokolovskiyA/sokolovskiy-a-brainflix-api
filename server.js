873e264a3424c632fb28fde735d5173c04382061
Alexander Sokolovskiy <sokolovskiya12@gmail.com>
2022-11-20 10:12:39 -0800

    added multer midlewear for upload functionality. files stored with unique name and assigned to a newVideo object. Upload functionality finished

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const port = 5000;
 const fs = require ('fs-extra');
 const {v4: uuid} = require('uuid');
 const cors = require("cors");
+const multer = require('multer')
 
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
+///MULTER MIDDLEWEAR TO STORE UPLOADED THUMBNAIL
+const storage = multer.diskStorage ({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images') ///Storage PATH
+    },
+    filename: function (req, file, cb) {
+        cb(null, uuid() + "_" + file.originalname) //UNIQUIE FILE NAME CONSTRUCTION
+    }
+})
+const upload = multer({ storage: storage }) ///SET STORAGE 
 
+///FUNCTIONS////
 function loadVideos() {
     const videos = fs.readFileSync('./data/video-details.json', 'utf8');
     return JSON.parse(videos);
@@ -22,7 +34,6 @@ function addNewVideo(newVideo) {
 function deleteComment(videoId, commentId) {
     const videos = loadVideos()
     const video = videos.find(video => video.id === videoId);
-    console.log(video.comments)
     const commentIndex = video.comments.findIndex(comment => comment.id === commentId);
     video.comments.splice(commentIndex, 1)
     fs.writeFile('./data/video-details.json', JSON.stringify(videos))
@@ -35,12 +46,19 @@ function addComment(videoId, newComment) {
     return(video.comments)
 }
 
-
-
+/*
+function addLike (videoId, commentId) {
+    const videos = loadVideos()
+    const video = videos.find(video => video.id === videoId);
+    const comment = video.comments.find(comment => comment.id === commentId)
+    return(video.comments)
+}
+*/
 
 app.get('/', (req, res) => {
     res.status(200).send("Hello. And  welcome  to  BrainFlix  API. ")
 });
+
 ////getting video list ****WORKS****
 app.get('/videos', (req, res) => {
     fs.readFile(`./data/video-details.json`, (err, videos)=>{
@@ -48,16 +66,18 @@ app.get('/videos', (req, res) => {
             res.status(500).json({message: "something went wrong", error: err})
         }
         else {
-            res.status(200).json(loadVideos());
+            res.status(200).json(JSON.parse(videos));
         }
     })
 });
 ////Posting new video ****WORKS****
-app.post('/videos', (req,res) => {
+app.post('/videos', upload.single('image') , (req,res) => {
     if (req.body.title && req.body.description) {
         let newVideo = {
             title: req.body.title,
-            channel: 'new channel', 
+            channel: 'User channel name', 
+            image: `http://localhost:5000/images/${req.file.filename}`,  //PATH TO UNIQUE FILENAME
+            video: "http://localhost:5000/videos/BrainStation.mp4",
             description: req.body.description, 
             timestamp: Date.now(),
             likes: 0, 
@@ -70,6 +90,7 @@ app.post('/videos', (req,res) => {
         res.status(400).send('Please provide video title and video description');
     }
 })
+
 /////getting video by id  *** WORKS ***
 app.get('/videos/:videoId', (req, res) => {
     fs.readFile(`./data/video-details.json`, (err, videosInfo)=>{
@@ -82,7 +103,7 @@ app.get('/videos/:videoId', (req, res) => {
         }
     });
 });
-///POSTIN COMMENTS ***WORKS***
+///POSTING COMMENTS ***WORKS***
 app.post('/videos/:videoId/comments', (req, res) => {
     if(req.params.videoId && req.body.comment && req.body.name) {
         let newComment = {
@@ -107,6 +128,11 @@ app.delete('/videos/:videoId/comments/:commentId', (req, res) => {
         res.status(404).send("comment not found");
     }
 });
+
+
+/*app.put('/videos/:videoId/comments/:commentId', (req, res) => {
+    res.send(addLike(req.params.videoId, req.params.commentId))
+});*/
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
